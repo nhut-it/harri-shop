b7980e4a6aa0f867d8da58a17fb35aea9b485dbe
nhut <it.nhutnguyen@gmail.com>
2023-04-12 13:02:09 +0700

    completed category home

---
+++ src/pages/Home/CardTopic/CardTopic.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const CardTopic = ({ img, data }) => {
+  return (
+    <a href="">
+      <img src={img} alt="computer" className="w-full" />
+      <p className="py-[21px] text-center text-[16px] font-bold text-content">
+        {data}
+      </p>
+    </a>
+  );
+};
+
+export default CardTopic;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,81 @@
 import React from "react";
 import HomeCarousel from "../../components/HomeCarousel/HomeCarousel";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/scrollbar";
+import "swiper/css/pagination";
+import { Keyboard, Scrollbar } from "swiper";
+import CardTopic from "./CardTopic/CardTopic";
+import "./Home.css";
+import computer from "../../assets/imgs/computer.webp";
+import exercise from "../../assets/imgs/exercise.webp";
+import spinning from "../../assets/imgs/spinning.webp";
+import ipad from "../../assets/imgs/ipad.webp";
+import camera from "../../assets/imgs/camera-blu.webp";
+import vr from "../../assets/imgs/vr.webp";
+import watches from "../../assets/imgs/watches.webp";
 
 const Home = () => {
   return (
     <div>
       <HomeCarousel />
+      <div className="slide-topic py-[100px]  bg-[#FFFF]">
+        <div className="container mx-auto">
+          <Swiper
+            slidesPerView={1}
+            centeredSlides={false}
+            slidesPerGroupSkip={1}
+            grabCursor={true}
+            spaceBetween={26}
+            loop={true}
+            keyboard={{
+              enabled: true,
+            }}
+            breakpoints={{
+              576: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+              },
+              768: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+              },
+              1024: {
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+              },
+            }}
+            scrollbar={true}
+            modules={[Keyboard, Scrollbar]}
+            className="mySwiper "
+          >
+            <SwiperSlide>
+              <CardTopic img={computer} data={" Computers Monitor & Laptop"} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <CardTopic
+                img={exercise}
+                data={" Exercise Bike & Shaver Clean"}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <CardTopic img={spinning} data={" Spinning Reel & Kettle"} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <CardTopic img={camera} data={" Camera Bluetooth & Headset"} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <CardTopic img={ipad} data={"Ipad Phone & Tablets"} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <CardTopic img={vr} data={" Planer & Virtual"} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <CardTopic img={watches} data={" Wireless & Watches"} />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
     </div>
   );
 };
